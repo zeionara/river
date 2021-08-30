033808bc13bcd2fdee38dff9e992f28f2ac4a3bf
Dmitrii <zeionara@gmail.com>
2021-08-30 23:37:36 +0300

    Implemented column groups allowing to merge values from multiple columns to obtain a single line

--- source/utils.r
+++ source/utils.r
@@ -10,12 +10,68 @@ get_property_value <- function(container, property_name, default = NULL) {
     return(value)
 }
 
+group_names_separator <- "::"
+
+get_property_value_as_reversed_string_map <- function(container, property_name, default = NULL) {
+    value <- get_property_value(container, property_name, default = default)
+    if (is.null(value)) {
+        return(NULL)
+    }
+
+    result <- hashmap(c("foo"), c("bar"))
+
+    group_names <- names(value)
+    for(i in seq_along(value)) {
+        for(column_name in value[[i]]){
+            if(!result$has_key(column_name)){
+                result[[column_name]] <- group_names[[i]]
+            } else {
+                result[[column_name]] <- paste(result[[column_name]], group_names[[i]], sep=group_names_separator)
+            }
+        }
+    }
+
+    result$erase("foo")
+    return(result)
+}
+
+group_columns <- function(data, manifest){
+    column_groups = get_property_value_as_reversed_string_map(manifest, 'column-groups')
+    if(is.null(column_groups)){
+        return(data)
+    }
+    input_data <- data
+    # print(column_groups)
+
+    
+    for(column_name in column_groups$keys()){
+        data_without_target_rows <- subset(data, data$variable != column_name)
+        for(group_name in str_split(column_groups[[column_name]], group_names_separator)[[1]]){
+            # print(paste(column_name, '->', group_name))
+            # eval(paste("data$", group_name, " <- ", "data$", column_name, sep=""))
+            # print(paste("data$", group_name, " <- ", "data$", column_name, sep=""))
+            # data[,group_name] <- data[,column_name]
+            target_rows <- data[data$variable == column_name,]
+            target_rows$variable <- group_name
+            data_without_target_rows <- rbind(data_without_target_rows, target_rows)
+        }
+        data <- data_without_target_rows
+    }
+   
+    # print(nrow(input_data))
+    # print(nrow(data_without_target_rows))
+    # print(nrow(data))
+
+    return(data)
+}
+
 visualize_corpus_using_line_plot <- function(path, images_path) {
     library(yaml)
     library(stringr)
     library(ggplot2)
     library(hrbrthemes)
     library(reshape2)
+    library(hashmap)
 
     # Generate image path
 
@@ -31,12 +87,14 @@ visualize_corpus_using_line_plot <- function(path, images_path) {
 
     data_path <- paste(path, manifest['data'], sep = "/")
     data <- read.table(file = data_path, sep = '\t', header = TRUE)
+    # group_columns(data, manifest)
     
     # print(head(data))
     # print(manifest['index-column'][[1]])
     
     melted <- melt(data, id=manifest['index-column'][[1]])
     names(melted)[1] <- 'id'
+    melted <- group_columns(melted, manifest)
 
     # print(head(melted))
 
@@ -51,7 +109,7 @@ visualize_corpus_using_line_plot <- function(path, images_path) {
     # ggplot(data, aes(x = c)) +
         (
             if(manifest['smooth'][[1]])
-            geom_smooth(size=1, alpha=0.4, linetype=1, show.legend=!manifest['disable-legend'][[1]])
+            geom_smooth(size=1, alpha=0.4, linetype=1, show.legend=!manifest['disable-legend'][[1]], level=0.98)
             else
             geom_line(size=1, alpha=0.9, linetype=1, show.legend=!manifest['disable-legend'][[1]])
         ) +
